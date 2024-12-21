e773f610c8e2ebc22029bbba6380d40993b81080
AyaHdf <aya.hdf24@gmail.com>
2024-12-21 20:59:57 +0100

    4th commit: 5 user stories MVP

--- lib/Screens/home.page.dart
+++ lib/Screens/home.page.dart
@@ -20,37 +20,46 @@ class HomePage extends StatelessWidget {
 
           final clothes = snapshot.data!.docs;
 
-return ListView.builder(
-  itemCount: clothes.length,
-  itemBuilder: (context, index) {
-    final item = clothes[index];
-    final data = item.data() as Map<String, dynamic>;
-          return Card(
-              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-              child: ListTile(
-                leading: Image.network(
-                  data['image'],
-                  width: 50,
-                  height: 50,
-                  fit: BoxFit.cover,
+          return ListView.builder(
+            itemCount: clothes.length,
+            itemBuilder: (context, index) {
+              final item = clothes[index];
+              final data = item.data() as Map<String, dynamic>;
+              return Card(
+                margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: ListTile(
+                  leading: Image.network(
+                    data['image'],
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.cover,
+                  ),
+                  title: Text(data['title']),
+                  subtitle: Text('Taille : ${data['size']} - Prix : ${data['price']} €'),
+                  onTap: () {
+                    Navigator.pushNamed(
+                      context,
+                      '/details',
+                      arguments: data,
+                    );
+                  },
                 ),
-                title: Text(data['title']),
-                subtitle: Text('Taille : ${data['size']} - Prix : ${data['price']} €'),
-                onTap: () {
-                  Navigator.pushNamed(
-                    context,
-                    '/details',
-                    arguments: data,
-                  );
-                },
-              ),
-            );
-          },
-        );
-
+              );
+            },
+          );
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0, // L'index par défaut de la page
+        onTap: (index) {
+          if (index == 1) {
+            // Naviguer vers la page panier
+            Navigator.pushNamed(context, '/basket');
+          } else if (index == 2) {
+            // Naviguer vers la page profil
+            Navigator.pushNamed(context, '/profile');
+          }
+        },
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.shop), label: 'Acheter'),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: 'Panier'),
